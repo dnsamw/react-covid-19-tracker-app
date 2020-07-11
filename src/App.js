263261ce77da2ec23a0e06abab7bd0e088eaccc7
Dileepa Samarawickrama <dansamarawicky@outlook.com>
2020-07-12 02:37:52 +0530

    api setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
+import { fetchData } from "./api/index";
 
 class App extends React.Component {
+  async componentDidMount() {
+    const data = await fetchData();
+    console.log(data);
+  }
   render() {
     return (
-      <div>
-        <h1>App</h1>
+      <div className={styles.container}>
+        <Cards />
+        <Chart />
+        <CountryPicker />
       </div>
     );
   }
---
+++ src/api/index.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const url = "https://covid19.mathdro.id/api";
+
+const fetchData = async () => {
+  try {
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(url);
+    return { confirmed, recovered, deaths, lastUpdate };
+  } catch (error) {}
+};
+export { fetchData };
